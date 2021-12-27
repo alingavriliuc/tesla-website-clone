1eaa4d37ee33c1aefe0d8589beb9489ba41b719f
alin <alingavriliuc@hotmail.com>
2021-12-27 21:48:23 +0100

    Added sidebar menu

--- tesla-clone/src/components/Header.js
+++ tesla-clone/src/components/Header.js
@@ -1,24 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@material-ui/icons/Menu'
+import CloseIcon from '@material-ui/icons/Close';
+import { selectCars } from '../features/carSlice/carSlice'
+import { useSelector } from 'react-redux'
 
 function Header() {
+
+    const [burgerStatus, setBurgerStatus] = useState(false);
+    const cars = useSelector(selectCars);
+
     return (
         <Container>
             <a>
                 <img src="/images/logo.svg" />
             </a>
             <Menu>
-                <a href="#">Model S</a>
-                <a href="#">Model 3</a>
-                <a href="#">Model X</a>
-                <a href="#">Model Y</a>
+                {cars && cars.map((car, index)=>(
+                    <a key={index} href="#">{car}</a>
+                ))}
             </Menu>
             <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Tesla Account</a>
-                <CustomMenu/>
+                <CustomMenu onClick={() => {
+                    if(!burgerStatus){
+                        setBurgerStatus(true);
+                    } 
+                }}/>
             </RightMenu>
+            <BurgerNav show={burgerStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick={() => {
+                        if(burgerStatus){
+                            setBurgerStatus(false);
+                        }
+                    }}/>
+                </CloseWrapper>
+                <li><a href="#">Existing Inventory</a></li>
+                <li><a href="#">Used Inventory</a></li>
+                <li><a href="#">Trade-in</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadster</a></li>
+            </BurgerNav>
         </Container>
     )
 }
@@ -36,6 +60,7 @@ const Container = styled.div`
     top: 0;
     left: 0;
     right: 0;
+    z-index: 1 ;
 `
 
 const Menu = styled.div`
@@ -68,4 +93,42 @@ const RightMenu = styled.div`
 
 const CustomMenu = styled(MenuIcon)`
     cursor: pointer;
+`
+
+const CustomClose = styled(CloseIcon)`
+    cursor: pointer;
+`
+
+const BurgerNav = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background: white;
+    width: 300px;
+    z-index: 100;
+    list-style: none;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+    transition: transform 0.5s ease-in-out;
+    transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+    li{
+        padding: 15px 0;
+        border-bottom: 1px solid rgba(0,0,0, .2);
+        a{
+            font-weight: 600;
+            transition: 0.5s all;
+        }
+        a:hover{
+            padding-left: 5px;
+        }
+
+    }
+`
+const CloseWrapper = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: end;
 `
--- tesla-clone/src/components/Section.js
+++ tesla-clone/src/components/Section.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import Fade from 'react-reveal/Fade'
-
+import { Fade } from 'react-reveal'
 function Section(props) {
     return (
         <Wrap bgImage = {props.backgroundImage}>
